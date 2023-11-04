69bb4b792e4e7fb10901d1240e528e50177bdd46
TobyJK <TobyJK@users.noreply.github.com>
2023-11-04 17:35:15 +0000

    Full chess logic

--- main.py
+++ main.py
@@ -126,11 +126,65 @@ class bishop(piece):
         self.letter = "B"
         self.points = 3
 
+    def listMoves(self):
+        for i in range(8):
+            if self in board[i]:
+                position = [i, board[i].index(self)]
+                break
+        
+        positions = []
+        new = [[1, 1], [1, -1], [-1, -1], [-1, 1]]
+        for thing in new:
+            current = [x for x in position]
+            while True:
+                current[0] += thing[0]
+                current[1] += thing[1]
+                if current[0] < 0 or current[1] < 0:
+                    break
+                try:
+                    if not board[current[0]][current[1]]:
+                        positions.append([x for x in current])
+                    elif (self.colour == 0 and board[current[0]][current[1]].colour == 1) or (self.colour == 1 and board[current[0]][current[1]].colour == 0):
+                        positions.append([x for x in current])
+                        break
+                    else:
+                        break
+                except IndexError:
+                    break
+        return positions
+    
 class queen(piece):
     def __init__(self, colour):
         super().__init__(colour)
         self.letter = "Q"
         self.points = 9
+    
+    def listMoves(self):
+        for i in range(8):
+            if self in board[i]:
+                position = [i, board[i].index(self)]
+                break
+        
+        positions = []
+        new = [[1, 1], [1, -1], [-1, -1], [-1, 1], [0, 1], [1, 0], [0, -1], [-1, 0]]
+        for thing in new:
+            current = [x for x in position]
+            while True:
+                current[0] += thing[0]
+                current[1] += thing[1]
+                if current[0] < 0 or current[1] < 0:
+                    break
+                try:
+                    if not board[current[0]][current[1]]:
+                        positions.append([x for x in current])
+                    elif (self.colour == 0 and board[current[0]][current[1]].colour == 1) or (self.colour == 1 and board[current[0]][current[1]].colour == 0):
+                        positions.append([x for x in current])
+                        break
+                    else:
+                        break
+                except IndexError:
+                    break
+        return positions
 
 class king(piece):
     def __init__(self, colour):
@@ -162,7 +216,7 @@ class king(piece):
 board = [[rook(0), knight(0), bishop(0), queen(0), king(0), bishop(0), knight(0), rook(0)], 
          [pawn(0), pawn(0), pawn(0), pawn(0), pawn(0), pawn(0), pawn(0), pawn(0)], 
          [None, None, None, None, None, None, None, None], 
-         [None, None, None, None, rook(0), None, None, None], 
+         [None, None, None, None, queen(0), None, None, None], 
          [None, None, None, None, None, None, None, None], 
          [None, None, None, None, None, None, None, None], 
          [pawn(1), pawn(1), pawn(1), pawn(1), pawn(1), pawn(1), pawn(1), pawn(1)], 
